b5e22b8137dde1a0b684120b869c6718b95bd56b
Chy-Zaber-Bin-Zahid <czaber47@gmail.com>
2025-07-21 10:28:23 +0600

    # Feature - CRUD operations done - Database connection done

---
+++ server/src/books/books.service.ts
@@ -0,0 +1,74 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { PG_CONNECTION } from 'src/constants';
+import { CreateBookDto } from './dto/create-book.dto';
+import { UpdateBookDto } from './dto/update-book.dto';
+
+@Injectable()
+export class BooksService {
+  constructor(@Inject(PG_CONNECTION) private conn: any) {}
+
+  async create(createBookDto: CreateBookDto) {
+    try {
+      const res = await this.conn.query(
+        'insert into books (book) values ($1) returning *',
+        [createBookDto.book],
+      );
+      console.log(res.rows[0]);
+      return {
+        statusCode: 201,
+        message: 'Book created successfully',
+        data: res.rows[0],
+      };
+    } catch (error) {
+      throw new Error(`Failed to create book: ${error}`);
+    }
+  }
+
+  async findAll() {
+    try {
+      const res = await this.conn.query('select * from books');
+      console.log(res.rows);
+      return {
+        statusCode: 200,
+        message: 'Book fetched successfully',
+        data: res.rows,
+      };
+    } catch (error) {
+      throw new Error(`Failed to create book: ${error}`);
+    }
+  }
+
+  async update(id: number, updateBookDto: UpdateBookDto) {
+    try {
+      const res = await this.conn.query(
+        'update books set book = ($1) where id = ($2) returning *',
+        [updateBookDto.book, id],
+      );
+      console.log(res.rows[0]);
+      return {
+        statusCode: 200,
+        message: 'Book updated successfully',
+        data: res.rows[0],
+      };
+    } catch (error) {
+      throw new Error(`Failed to create book: ${error}`);
+    }
+  }
+
+  async remove(id: number) {
+    try {
+      const res = await this.conn.query(
+        'delete from books where id = ($1) returning *',
+        [id],
+      );
+      console.log(res.rows[0]);
+      return {
+        statusCode: 200,
+        message: 'Book deleted successfully',
+        data: res.rows[0],
+      };
+    } catch (error) {
+      throw new Error(`Failed to create book: ${error}`);
+    }
+  }
+}
